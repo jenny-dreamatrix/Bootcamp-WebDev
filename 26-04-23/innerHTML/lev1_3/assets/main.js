825a2df40c214731d64256a195940ee249fd73ea
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-04-26 12:54:51 +0200

    new

--- 26-04-23/innerHTML/lev1_3/assets/main.js
+++ 26-04-23/innerHTML/lev1_3/assets/main.js
@@ -6,3 +6,6 @@ console.log(gallery);
 gallery.innerHTML += "<figure><img src=\"https://www.wwf.de/fileadmin/_processed_/3/7/csm_leopard-massai-mara-kenia-WW22416-c-naturepl-com-Anup-Shah-WWF_79b7762679.jpg\" width=\"300px\" height=\"200px\"><p>Fig. 1</p></figure>";
 gallery.innerHTML += "<figure><img src=\"https://www.wwf.de/fileadmin/_processed_/3/7/csm_leopard-massai-mara-kenia-WW22416-c-naturepl-com-Anup-Shah-WWF_79b7762679.jpg\" width=\"300px\" height=\"200px\"><p>Fig. 2</p></figure>";
 gallery.innerHTML += "<figure><img src=\"https://www.wwf.de/fileadmin/_processed_/3/7/csm_leopard-massai-mara-kenia-WW22416-c-naturepl-com-Anup-Shah-WWF_79b7762679.jpg\" width=\"300px\" height=\"200px\"><p>Fig. 3</p></figure>";
+
+gallery.style.display = "flex";
+gallery.style.justifyContent = "center";
