5cfc756e1583e3ed65eb0c9e6ea51ca5976f81ca
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-04-28 13:28:28 +0200

    updated

--- 28-04-23/if else/lev2_3/assets/js/main.js
+++ 28-04-23/if else/lev2_3/assets/js/main.js
@@ -1,11 +1,10 @@
 
-
 function calculate(){
     let zahl = Number(document.querySelector("#zufallszahl").value);
 
     let differenz = zahl - 27;
 
     if (differenz > 27){
-        console.log("multiply " + differenz*2);
+        console.log("multiply ", differenz*2);
     } 
 }
