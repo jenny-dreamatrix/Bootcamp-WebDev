67d7f4922ce7be887ca8f2f894d9dfe2f6d1f378
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-05-02 15:35:10 +0200

    updated comments

--- 02-05-23/string methods/lev2_3/assets/js/main.js
+++ 02-05-23/string methods/lev2_3/assets/js/main.js
@@ -4,7 +4,7 @@ const output = document.querySelector(".output");
 
 
 function highlight(){
-    // die <span> Elemente und den output Text aus der vorherigen Suche entfernen, alles wieder zurücksetzen auf ursprünglichen Zustand
+    // die <span> Elemente und den output Text aus der vorherigen Suche entfernen, alles wieder zurücksetzen auf den ursprünglichen Zustand
     article.innerHTML = article.innerHTML.replaceAll("<span>", " ");
     output.innerHTML = " ";
 
@@ -12,22 +12,27 @@ function highlight(){
     let search = document.querySelector("#search-input").value;
     console.log(search);
 
-    // überprüfen, ob in dem Text des articles das Suchwort enthalten ist - als Variable
+    // überprüfen, ob in dem Text des articles das Suchwort enthalten ist - als Variable speichern
     let result = article.innerText.includes(search);
+    // gibt true oder false raus
     console.log(result);
 
     // wenn das Suchwort enthalten ist,..
     if(result == true){
+        // Variable, die das Suchwort in <span> Element packt
         let addSpan = "<span>" + search + "</span>";
 
+        // innerHTML im ursprünglichen Zustand
         console.log(article.innerHTML);  
 
         // alle search Ergebnisse ersetzen und in der Konsole überprüfen ob die Änderungen übernommen wurden
-        let replaced = article.innerHTML.replaceAll(search, addSpan);
-        console.log(replaced);
+        // let replaced = article.innerHTML.replaceAll(search, addSpan);
+        // console.log(replaced);
 
         // das innerHTML überschreiben
         article.innerHTML = article.innerHTML.replaceAll(search, addSpan);
+        
+        // wenn das Suchwort nicht enthalten ist,..
     } else {
         output.innerHTML = "Keine Ergebnisse gefunden.";
     }
