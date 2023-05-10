70ecf4c79e9cfee50b6fbaa79414fafc63e5bd7b
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-05-10 13:25:09 +0200

    updated

--- 10-05-23/for loop/lev3_2/assets/js/main.js
+++ 10-05-23/for loop/lev3_2/assets/js/main.js
@@ -4,9 +4,9 @@ let numArr = [5, 22, 15, 100, 55];
 
 numArr.forEach((elm) => {
 
-    for (let i = 2; i <= elm; i++){
+    for (let i = 2; i < elm; i++){
         
-        if (elm % i === 0 && elm != i){
+        if (elm % i === 0){
             let ergebnis = elm / i;
             console.log(`${elm} lässt sich durch ${i} teilen! Das Ergebnis ist ${ergebnis}`);
             document.write(`<p>${elm} lässt sich durch ${i} teilen! Das Ergebnis ist ${ergebnis}</p>`);
