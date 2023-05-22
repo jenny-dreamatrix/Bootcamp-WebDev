47ca37b767077755fb81d8d6988be9292e353542
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-05-22 13:15:04 +0200

    updated JS

--- 22-05-23/date/lev1_2/assets/js/main.js
+++ 22-05-23/date/lev1_2/assets/js/main.js
@@ -24,6 +24,8 @@ let wochenTag = [
     "Samstag"
 ];
 
+// übungsaufgabe
+
 let date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth();
@@ -35,6 +37,8 @@ let monthName = monate[month];
 
 document.write(`<p>${date}</p><p>${year}</p><p>${month} Monat</p><p>${day} Tag</p><p>${hour} Stunde</p><p>${minute} Minute</p><p>${weekday}</p><p>${monthName}</p>`);
 
+// digitale uhr
+
 const dayOutput = document.querySelector(".day-output");
 const hourOutput = document.querySelector(".hour-output");
 const minutesOutput = document.querySelector(".minutes-output");
@@ -45,9 +49,21 @@ let weekdayShort = date.toLocaleString("default", {weekday: "short"});
 let seconds = date.getSeconds();
 let period = hour < 12 ? "AM" : "PM";
 
+if(hour < 10){
+    hour = hour.toString();
+    hour = `0${hour}`
+}
+if(minute < 10){
+    minute = minute.toString();
+    minute = `0${minute}`
+}
+if(seconds < 10){
+    seconds = seconds.toString();
+    seconds = `0${seconds}`
+}
+
 dayOutput.innerHTML = weekdayShort;
 hourOutput.innerHTML = hour;
 minutesOutput.innerHTML = minute;
 secondsOutput.innerHTML = seconds;
 periodOutput.innerHTML = period;
-
