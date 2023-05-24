cfb33f7309b470a6612c5816bdbdec970f9aebcc
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-05-24 14:43:13 +0200

    updated JS

--- 24-05-23/BOM/lev1_2/assets/js/main.js
+++ 24-05-23/BOM/lev1_2/assets/js/main.js
@@ -2,15 +2,18 @@
 const button = document.querySelector("#btn");
 const zeit = document.querySelector(".zeit");
 
+let interval;
 let count = 100;
 
 const counter = () => {
-    let interval = setInterval(() => {
-        if(count <= 100 && count >= 0){
-            zeit.innerHTML = `${count}%`;
-            count--;
-        } else {
-            clearInterval(interval)
-        }
-        }, 100);
+    interval = setInterval(counterNew, 100);
+}
+
+const counterNew = () => {
+    if(count <= 100 && count >= 0){
+        zeit.innerHTML = `${count}%`;
+        count--;
+    } else {
+        clearInterval(interval);
+    }
 }
