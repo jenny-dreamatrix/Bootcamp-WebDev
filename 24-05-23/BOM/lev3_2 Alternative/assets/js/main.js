75902bb97ef0fab6d0c8def4b92794838a34b9d2
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-05-24 16:46:53 +0200

    comments

--- 24-05-23/BOM/lev3_2 Alternative/assets/js/main.js
+++ 24-05-23/BOM/lev3_2 Alternative/assets/js/main.js
@@ -6,6 +6,7 @@ let timeCounter;
 const startMinCountdown = () => {
     const minutesInput = Number(document.querySelector("#minutes").value);
 
+    // in Millisekunden umrechnen
     timeCounter = minutesInput * 60 * 1000;
 
     interval = setInterval(countdown, 1000);
@@ -14,17 +15,21 @@ const startMinCountdown = () => {
 const countdown = () => {
 
     if(timeCounter >= 0){
+    // mit Math.floor rundet man ab auf ganze Zahl -> wenn also z.b. die minutes unter 2 gehen, wird 1 dort stehen 
     let minutes = Math.floor(timeCounter / (1000 * 60));
     let seconds = Math.floor(timeCounter / 1000);
     console.log({seconds}, {minutes});
 
     let minuteTime = minutes;
+    // verbleibende Sekunden sind die gesamte Sekundenanzahl minus die Minuten (in Sekunden umgerechnet)
     let secondsTime = seconds - minutes * 60;
     console.log({minuteTime}, {secondsTime});
 
+    // mit jedem Durchlauf wird 1 Sekunde abgezogen
     timeCounter -= 1000;
 
     time.innerHTML = `${minuteTime}:${secondsTime}`;
+    
     } else {
         clearInterval(interval);
     }
