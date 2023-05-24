7c3c1a87c4ced44253c6bad6170ab846484e0b85
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-05-24 14:46:03 +0200

    js

--- 24-05-23/BOM/lev1_2/assets/js/main.js
+++ 24-05-23/BOM/lev1_2/assets/js/main.js
@@ -5,11 +5,11 @@ const zeit = document.querySelector(".zeit");
 let interval;
 let count = 100;
 
-const counter = () => {
-    interval = setInterval(counterNew, 100);
+const intervalGo = () => {
+    interval = setInterval(counter, 100);
 }
 
-const counterNew = () => {
+const counter = () => {
     if(count <= 100 && count >= 0){
         zeit.innerHTML = `${count}%`;
         count--;
