1301abba8ea0bf5b4900b6fd0f6af86af087d21c
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-05-25 14:31:28 +0200

    alternative

--- 25-05-23/API/lev2_1/assets/js/main.js
+++ 25-05-23/API/lev2_1/assets/js/main.js
@@ -9,24 +9,23 @@ fetch("https://picsum.photos/v2/list")
         const author = beitrag.author;
         const image = beitrag.download_url;
 
-        const wrapper = document.createElement("figure");
+        // entweder so:
+        // const wrapper = document.createElement("figure");
 
-        const imgOfBeitrag = document.createElement("img");
-        imgOfBeitrag.setAttribute("src", image);
-        wrapper.appendChild(imgOfBeitrag);
+        // const imgOfBeitrag = document.createElement("img");
+        // imgOfBeitrag.setAttribute("src", image);
+        // wrapper.appendChild(imgOfBeitrag);
 
-        const authorOfBeitrag = document.createElement("figcaption");
-        authorOfBeitrag.textContent = author;
-        wrapper.appendChild(authorOfBeitrag);
+        // const authorOfBeitrag = document.createElement("figcaption");
+        // authorOfBeitrag.textContent = author;
+        // wrapper.appendChild(authorOfBeitrag);
 
-        document.querySelector("main").appendChild(wrapper);
+        // document.querySelector("main").appendChild(wrapper);
 
-
-        // const inhalt = `<img src="${image}"><figcaption>${author}</figcaption>`;
-        // wrapper.insertAdjacentHTML("afterbegin",inhalt);
-        // document.querySelector(".test").insertAdjacentHTML("afterbegin", wrapper);
+        // oder so:
+        const inhalt = `<figure><img src="${image}"><figcaption>${author}</figcaption></figure>`;
+        document.querySelector("main").insertAdjacentHTML("beforeend", inhalt);
     });
-
 })
 .catch((err) => console.log(`Fehler: ${err}`));
 
