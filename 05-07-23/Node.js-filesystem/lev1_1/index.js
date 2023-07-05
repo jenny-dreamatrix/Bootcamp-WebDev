4b67590a1ddd5b51e2d5ed015f2354057d979fd7
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-07-05 16:46:56 +0200

    added try catch to all promises

--- 05-07-23/Node.js-filesystem/lev1_1/index.js
+++ 05-07-23/Node.js-filesystem/lev1_1/index.js
@@ -1,47 +1,79 @@
 import fs from "node:fs/promises"
 
 // blog1 verändern
-const pathBlog1 = new URL("./blog1.txt", import.meta.url)
-const blog1Content = await fs.readFile(pathBlog1, {encoding: "utf8"})
-console.log({blog1Content});
-await fs.writeFile(pathBlog1, "Ich bin ein Developer", {encoding: "utf8"})
+try {
+    const pathBlog1 = new URL("./blog1.txt", import.meta.url)
+    const blog1Content = await fs.readFile(pathBlog1, {encoding: "utf8"})
+    console.log({blog1Content});
+    await fs.writeFile(pathBlog1, "Ich bin ein Developer", {encoding: "utf8"})
+    console.log(`blog1 verändert`);
+} catch (err) {
+    console.error(`Fehler: ${err}`);
+}
 
 // blog2 erstellen
-const pathBlog2 = new URL("./blog2.txt", import.meta.url)
-await fs.writeFile(pathBlog2, "beliebiger Text", {encoding: "utf8"})
-const blog2Content = await fs.readFile(pathBlog2, {encoding: "utf8"})
-console.log({blog2Content});
+try {
+    const pathBlog2 = new URL("./blog2.txt", import.meta.url)
+    await fs.writeFile(pathBlog2, "beliebiger Text", {encoding: "utf8"})
+    const blog2Content = await fs.readFile(pathBlog2, {encoding: "utf8"})
+    console.log({blog2Content});
+    console.log(`blog2 erstellt`);
+} catch (err) {
+    console.error(`Fehler: ${err}`);
+}
 
 // assets prüfen und ggf löschen
 const pathAssets = new URL("./assets", import.meta.url)
 try {
     await fs.access(pathAssets);
-    console.log("assets ist bereits vorhanden");
+    console.log(`assets ist bereits vorhanden`);
     await fs.rmdir(pathAssets)
+    console.log(`assets wurde gelöscht`);
 } catch (err) {
-    console.log("assets nicht vorhanden", err);
+    console.error(`Fehler: ${err}`);
 }
 
 // assets erstellen
-await fs.mkdir(pathAssets, { recursive: true })
+try {
+    await fs.mkdir(pathAssets, { recursive: true })
+    console.log(`assets wurde erstellt`);
+} catch (err) {
+    console.error(`Fehler: ${err}`);
+}
 
 // delete.txt prüfen und ggf. löschen
 const pathDelete = new URL("./delete.txt", import.meta.url)
 try {
     await fs.access(pathDelete)
-    console.log("delete ist bereits vorhanden");
+    console.log(`delete ist bereits vorhanden`);
     await fs.rm(pathDelete)
+    console.log(`delete wurde gelöscht`);
 } catch (err) {
-    console.log("delete ist noch nicht vorhanden", err);
+    console.error(`Fehler: ${err}`);
 }
 
 // delete.txt erstellen
-await fs.writeFile(pathDelete, "random text", {encoding: "utf8"})
+try {
+    await fs.writeFile(pathDelete, "random text", {encoding: "utf8"})
+    console.log(`delete wurde erstellt`);
+} catch (err) {
+    console.error(`Fehler: ${err}`);
+}
 
 // Hello.txt erstellen
 const pathHello = new URL("./Hello.txt", import.meta.url)
-await fs.writeFile(pathHello, "irgendwas", {encoding: "utf8"})
+try {
+    await fs.writeFile(pathHello, "irgendwas", {encoding: "utf8"})
+    console.log(`Hello wurde erstellt`);
+} catch (err) {
+    console.error(`Fehler: ${err}`);
+}
 
 // Hello.txt umbenennen
 const newPathHello = new URL("./HelloWorld.txt", import.meta.url)
-await fs.rename(pathHello, newPathHello)
+try {
+    await fs.rename(pathHello, newPathHello)
+    console.log(`Hello wurde umbenannt`);
+} catch (err) {
+    console.error(`Fehler: ${err}`);
+}
