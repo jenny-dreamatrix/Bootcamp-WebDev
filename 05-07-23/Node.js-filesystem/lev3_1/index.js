03631c1180d9bd57c5b1b0d3d750b4191c77336c
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-07-05 17:03:35 +0200

    added try catch

--- 05-07-23/Node.js-filesystem/lev3_1/index.js
+++ 05-07-23/Node.js-filesystem/lev3_1/index.js
@@ -1,20 +1,33 @@
 import fs from "node:fs/promises"
 
 const pathDir = new URL("./neu", import.meta.url)
-await fs.mkdir(pathDir, {recursive: true})
+try {
+    await fs.mkdir(pathDir, {recursive: true})
+    console.log(`Ordner wurde erstellt`);
+} catch (err) {
+    console.error(`Fehler: ${err}`)
+}
 
 const pathText = new URL("./neu/text.txt", import.meta.url)
 
 const checkFile = async (parameter) => {
 try {
     await fs.access(pathText)
-    console.log("Die Datei existiert bereits.");
-    await fs.appendFile(pathText, "\n" + parameter)
-    
+    console.log(`Die Datei existiert bereits.`)
+    try {
+        await fs.appendFile(pathText, "\n" + parameter)
+        console.log(`Neuer Inhalt wurde hinzugefügt`)
+    } catch (err) {
+        console.error(`Fehler von appendFile(): ${err}`);
+    }
 } catch (err) {
-    console.log("Die Datei existiert noch nicht.", err);
-    await fs.writeFile(pathText, parameter)
-}
-}
+    console.error(`Fehler von access(): ${err}`);
+    try {
+        await fs.writeFile(pathText, parameter)
+        console.log(`Datei wurde erstellt`);
+    } catch (err) {
+        console.error(`Fehler von writeFile(): ${err}`);
+    }
+}}
 
-checkFile("dies ist ein Test 4")
+checkFile("dies ist ein Test")
