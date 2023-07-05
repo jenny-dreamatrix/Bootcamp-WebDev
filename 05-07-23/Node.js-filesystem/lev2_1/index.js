89ba96c69c6a102ada0fc8bf207e978a7273ddc1
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-07-05 16:53:46 +0200

    added try catch

--- 05-07-23/Node.js-filesystem/lev2_1/index.js
+++ 05-07-23/Node.js-filesystem/lev2_1/index.js
@@ -4,19 +4,35 @@ const pathData = new URL("./data.json", import.meta.url)
 const pathText = new URL("./data.txt", import.meta.url)
 
 // reading data from data.json
-const contentData = await fs.readFile(pathData, {encoding: "utf8"})
-// console.log({contentData});
+let contentData;
+try {
+    contentData = await fs.readFile(pathData, {encoding: "utf8"})
+    // console.log({contentData});
+    console.log(`data.json wurde gelesen`);
+} catch (err) {
+    console.error(`Fehler: ${err}`)
+}
 
-// in json Format umwandeln
+// Daten in json Format umwandeln
 const contentJSON = JSON.parse(contentData)
 
 // eine leere Text Datei erstellen
-await fs.writeFile(pathText, "", {encoding: "utf8"})
+try {
+    await fs.writeFile(pathText, "", {encoding: "utf8"})
+    console.log(`Text Datei wurde erstellt`);
+} catch (err) {
+    console.error(`Fehler: ${err}`)
+}
 
 // Text Datei mit Inhalten füllen und formatieren
 const writingFunction = async (contentJSON) => {
     for (let i = 0; i < contentJSON.length; i++) {
-        await fs.appendFile(pathText, contentJSON[i]["id"] + " - " + contentJSON[i]["title"] + "\n" + contentJSON[i]["description"] + "\n" + "\n", {encoding: "utf8"})
+        try {
+            await fs.appendFile(pathText, contentJSON[i]["id"] + " - " + contentJSON[i]["title"] + "\n" + contentJSON[i]["description"] + "\n" + "\n", {encoding: "utf8"})
+            console.log(`Inhalte wurden der Text Datei hinzugefügt`);
+        } catch (err) {
+            console.error(`Fehler: ${err}`)
+        }
     }
 }
 writingFunction(contentJSON)
