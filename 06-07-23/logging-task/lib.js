3e5897e196f8334919e44b4e2b5f2f83e6e2cf97
jenny-dreamatrix <jenny.dreamatrix@gmail.com>
2023-07-06 14:20:50 +0200

    loggingtask

---
+++ 06-07-23/logging-task/lib.js
@@ -0,0 +1,38 @@
+import fs from "node:fs/promises"
+
+const pathLog = new URL("./logs/log.txt", import.meta.url)
+const pathInfo = new URL("./logs/info.txt", import.meta.url)
+const pathWarn = new URL("./logs/warn.txt", import.meta.url)
+const pathError = new URL("./logs/error.txt", import.meta.url)
+
+export const writeLog = async (logLevel, timeStamp, logMessage) => {
+    try{
+        await fs.appendFile(pathLog, logLevel + " :: " + timeStamp + " :: " + logMessage + "\n" )
+        console.log(`Daten wurden erfolgreich in log.txt geschrieben`);
+        if (logLevel === `Info`){
+            try {
+                await fs.appendFile(pathInfo, logLevel + " :: " + timeStamp + " :: " + logMessage + "\n" )
+                console.log(`Daten wurden erfolgreich in info.txt geschrieben`);
+            } catch (err){
+                console.error(`Fehler beim Schreiben in info.txt: ${err}`);
+            }
+        } else if (logLevel === `Warn`){
+            try {
+                await fs.appendFile(pathWarn, logLevel + " :: " + timeStamp + " :: " + logMessage + "\n" )
+                console.log(`Daten wurden erfolgreich in warn.txt geschrieben`);
+            } catch (err){
+                console.error(`Fehler beim Schreiben in warn.txt: ${err}`);
+            }
+        } else if (logLevel === `Error`){
+            try {
+                await fs.appendFile(pathError, logLevel + " :: " + timeStamp + " :: " + logMessage + "\n" )
+                console.log(`Daten wurden erfolgreich in error.txt geschrieben`);
+            } catch (err){
+                console.error(`Fehler beim Schreiben in error.txt: ${err}`);
+            }
+        }
+    } catch (err){
+        console.error(`Fehler beim Schreiben in log.txt: ${err}`);
+    }
+}
+
---
+++ 06-07-23/logging-task/main.js
@@ -0,0 +1,28 @@
+import { writeLog } from "./lib.js";
+
+const timeStamp = new Date().toUTCString()
+const userID = Math.floor(Math.random() * 99999999999)
+
+let login = "failed"
+let logMessage;
+let logLevel;
+
+if (login === "successful"){
+    logMessage = `User ${userID} Login Successful`
+    logLevel = `Log`
+    console.log(logMessage);
+} else if (login === "success-info") {
+    logMessage = `User ${userID} Login Successful`
+    logLevel = `Info`
+    console.info(logMessage)
+} else if (login === "success-warn"){
+    logMessage = `User ${userID} Login Successful`
+    logLevel = `Warn`
+    console.warn(logMessage)
+} else if (login === "failed"){
+    logMessage = `User ${userID} Login Failed`
+    logLevel = `Error`
+    console.error(logMessage)
+}
+
+writeLog(logLevel, timeStamp, logMessage)
